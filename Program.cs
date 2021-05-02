7643914b0683055c042320f4e26ff37beec0e110
IvanSenkin <83362469+IvanSenkin@users.noreply.github.com>
2021-05-02 10:16:22 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,38 +1,32 @@
 ﻿using System;
 
-namespace EX5
+namespace EX4
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //5.
-            //а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
-            //б) Сделать задание, только вывод организовать в центре экрана.
-            //в) *Сделать задание б с использованием собственных методов(например, Print(string ms, int x, int y).
+            //4.Написать программу обмена значениями двух переменных типа int без использования вспомогательных методов.
+            //а) с использованием третьей переменной;
+            //б) *без использования третьей переменной.
 
-            Console.Write("Введите имя: ");
-            string name = Console.ReadLine();
-            int centerX1 = (Console.WindowWidth / 2) - (name.Length / 2);
-            int centerY1 = (Console.WindowHeight / 2) - 3;
+            Console.Write("Введите первое число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a;
+            a = b;
+            b = c;
+            Console.WriteLine($"a) Первое число: {a}, второе число {b}");
 
-            Console.Write("Введите фамилию: ");
-            string surname = Console.ReadLine();
-            int centerX2 = (Console.WindowWidth / 2) - (surname.Length / 2);
-            int centerY2 = (Console.WindowHeight / 2) - 2;
+            // задание б)*
+            Console.Write("Введите первое число: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int y = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Введите город: ");
-            string city = Console.ReadLine();
-            int centerX3 = (Console.WindowWidth / 2) - (city.Length / 2);
-            int centerY3 = (Console.WindowHeight / 2) - 1;
-
-            Console.SetCursorPosition(centerX1, centerY1);
-            Console.WriteLine(name);
-            Console.SetCursorPosition(centerX2, centerY2);
-            Console.WriteLine(surname);
-            Console.SetCursorPosition(centerX3, centerY3);
-            Console.WriteLine(city);
-
+            (x, y) = (y, x);
+            Console.WriteLine($"б)* Первое число: {x}, второе число {y}");
         }
     }
 }
